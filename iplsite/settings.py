9d303543ede790751b54cf6eed6e63046eb73fc9
crazynayan <nayan@crazyideas.co.in>
2019-08-23 13:39:03 +0530

    Cosmetic changes

--- iplsite/settings.py
+++ iplsite/settings.py
@@ -27,7 +27,7 @@ with open('secret_key.txt') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '*' ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
